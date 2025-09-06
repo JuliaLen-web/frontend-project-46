becef1f59e16d94801f651df3335b639c8b5eb5f
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-09-06 17:26:50 +0400

    4 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,13 +2,15 @@
 
 import { Command } from 'commander'
 import readFiles from "../src/readFiles.js";
+import diff from "../src/diff.js";
 const program = new Command()
 
 program
   .arguments('[filepath1] [filepath2]') // Define a command with arguments
   .action((filepath1, filepath2) => {
-    readFiles(filepath1);
-    readFiles(filepath2);
+    const file1 = readFiles(filepath1);
+    const file2 = readFiles(filepath2);
+    diff(file1, file2);
 
     console.log(`File path 1: ${filepath1}`);
     console.log(`File path 2: ${filepath2}`);
---
+++ src/diff.js
@@ -0,0 +1,28 @@
+import _ from 'lodash' // Импорт Lodash
+
+export default function diff(obj1, obj2) {
+  const diff = {}
+
+  for (const [key, value] of Object.entries(obj1)) {
+    if (Object.hasOwn(obj2, key)) {
+      if (obj2[key] === value) {
+        diff[key] = value
+      }
+      else {
+        diff[`+ ${key}`] = value
+        diff[`- ${key}`] = value
+      }
+    }
+    else {
+      diff[`- ${key}`] = value
+    }
+  }
+
+  for (const [key, value] of Object.entries(obj2)) {
+    if (!(Object.hasOwn(obj1, key))) {
+      diff[`+ ${key}`] = value
+    }
+  }
+  console.log(diff)
+  return diff
+}
