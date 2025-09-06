21bb43ee158425f5e769780fbe2f242011c6cc48
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-09-06 11:30:54 +0400

    2 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
-import { program } from 'commander'
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+const program = new Command()
 
 program
+  .arguments('<filepath1> <filepath2>') // Define a command with arguments
+  .action((filepath1, filepath2) => {
+    console.log(`Argument 1: ${filepath1}`);
+    console.log(`Argument 2: ${filepath2}`);
+  })
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+
+program.parse(process.argv);
