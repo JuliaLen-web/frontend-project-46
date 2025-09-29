7f805fcb0f2e2e0ba17a1699173575d95f28fe08
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-09-29 14:33:32 +0400

    5 step

--- src/readFiles.js
+++ src/readFiles.js
@@ -1,7 +1,6 @@
 import { readFileSync } from 'node:fs';
 
 export default function readFiles(file) {
-  readFileSync(file);
-  // console.log(JSON.parse(readFileSync(file)))
-  return JSON.parse(readFileSync(file));
+  readFileSync(file)
+  return JSON.parse(readFileSync(file))
 }
