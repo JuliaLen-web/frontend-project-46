e88c828344b5a0b22d6c36fc250a8b7202ba7e98
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-10-01 21:52:42 +0400

    from CRLF to LF

--- src/diff.js
+++ src/diff.js
@@ -1,36 +1,4 @@
-// import parsers from "./parsers.js"
 import _ from 'lodash'
-
-// export default function (file1, file2) {
-//   return diff(parsers(file1), parsers(file2))
-// }
-
-// function diff(obj1, obj2) {
-//   const diff = {}
-//
-//   for (const [key, value1] of Object.entries(obj1)) {
-//     if (Object.hasOwn(obj2, key)) {
-//       if (obj2[key] === value1) {
-//         diff[key] = value1
-//       }
-//       else {
-//         diff[`- ${key}`] = value1
-//         diff[`+ ${key}`] = obj2[key]
-//       }
-//     }
-//     else {
-//       diff[`- ${key}`] = value1
-//     }
-//   }
-//
-//   for (const [key, value2] of Object.entries(obj2)) {
-//     if (!(Object.hasOwn(obj1, key))) {
-//       diff[`+ ${key}`] = value2
-//     }
-//   }
-//
-//   return JSON.stringify(diff)
-// }
 
 export default function diff(data1, data2) {
   const keys = _.union(Object.keys(data1), Object.keys(data2))
