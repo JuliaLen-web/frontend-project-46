800b2f6dc2f849b67516eea2eda8f144f88c649f
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-10-02 11:52:53 +0400

    test

--- src/parsers.js
+++ src/parsers.js
@@ -5,9 +5,9 @@ import yaml from 'js-yaml'
 export default function (filepath) {
   const format = path.extname(filepath)
   if (format === '.json') {
-    return JSON.parse(readFileSync(filepath, 'utf-8'))
+    return JSON.parse(readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'))
   }
   else {
-    return yaml.load(readFileSync(filepath, 'utf-8'))
+    return yaml.load(readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'))
   }
 }
